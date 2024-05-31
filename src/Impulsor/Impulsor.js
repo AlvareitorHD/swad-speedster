cf06b6df3f53edcc78f217534df5a61bd8c40cb1
AlvareitorHD <alvareitor.14.03@gmail.com>
2024-05-31 20:12:05 +0200

    hecho impulsor

--- src/Impulsor/Impulsor.js
+++ src/Impulsor/Impulsor.js
@@ -2,99 +2,83 @@ import * as THREE from '../../libs/three.module.js'
 
 import { CSG } from '../../libs/CSG-v2.js'
 
-class Impulsor extends THREE.Object3D{
-    constructor(gui,titleGui) {
-        super();
+class Impulsor extends THREE.Object3D {
+  constructor(gui, titleGui, c, t, rot) {
+    super();
+    var circuito = c.tubeGeometry;
+    //Creamos los materiales
 
-        // Se crea la parte de la interfaz que corresponde a la caja
-        // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
-        this.createGUI(gui,titleGui);
+    this.video = document.createElement('video');
+    this.video.crossOrigin = 'anonymous';
+    this.video.preload = '';
+    this.video.loop = true;
+    this.video.src = '/imgs/flechas.mp4';
+    this.video.load();
+    var texture = new THREE.VideoTexture(this.video);
+    texture.generateMipmaps = false; // si el video no es cuadrado
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.format = THREE.RGBAFormat;
 
-        //Creamos los materiales
-        var texture = new THREE.TextureLoader().load('../../imgs/cono_trafico.jpeg');
-        var Mat = new THREE.MeshStandardMaterial({map: texture});
-        
-        //Creamos las geometrias
-        var base = new THREE.BoxGeometry(2, 0.05, 1);
+    // Crear el material con la textura de vídeo y la emisión
+    const Mat = new THREE.MeshStandardMaterial({
+      map: texture,
+      emissive: new THREE.Color(0xffffff), // Color de la emisión
+      emissiveIntensity: 1, // Intensidad de la emisión
+      emissiveMap: texture // Textura de emisión
+    });
 
-        //Se posiciona y se orienta
+    //Creamos las geometrias
+    var base = new THREE.BoxGeometry(2, 0.05, 1);
 
-        // Un Mesh se compone de geometría y material
-        var baseMesh = new THREE.Mesh(base, Mat);
+    //Se posiciona y se orienta
 
-        //Creamos el csg
-        var csg = new CSG();
-        csg.union([baseMesh]);
+    // Un Mesh se compone de geometría y material
+    this.baseMesh = new THREE.Mesh(base, Mat);
 
-        this.cono_trafico = csg.toMesh();
-        // Y añadirlo como hijo del Object3D (el this)
-        this.add(this.cono_trafico);
-    }
+    // Y añadirlo como hijo del Object3D (el this)
+    this.add(this.baseMesh);
+    this.video.play();
+    this.posicionar(circuito, t, rot);
+    this.createColision();
+  }
 
-    createGUI (gui,titleGui) {
-        // Controles para el tamaño, la orientación y la posición de la caja
-        this.guiControls = {
-          sizeX : 1.0,
-          sizeY : 1.0,
-          sizeZ : 1.0,
-          
-          rotX : 0.0,
-          rotY : 0.0,
-          rotZ : 0.0,
-          
-          posX : 0.0,
-          posY : 0.0,
-          posZ : 0.0,
-          
-          // Un botón para dejarlo todo en su posición inicial
-          // Cuando se pulse se ejecutará esta función.
-          reset : () => {
-            this.guiControls.sizeX = 1.0;
-            this.guiControls.sizeY = 1.0;
-            this.guiControls.sizeZ = 1.0;
-            
-            this.guiControls.rotX = 0.0;
-            this.guiControls.rotY = 0.0;
-            this.guiControls.rotZ = 0.0;
-            
-            this.guiControls.posX = 0.0;
-            this.guiControls.posY = 0.0;
-            this.guiControls.posZ = 0.0;
-          }
-        }
+  posicionar(circuito,ti,rot){
+    this.baseMesh.geometry.rotateY(-Math.PI/2);
+    this.nodoPosOrientTubo = new THREE.Object3D();
+    this.movimientoLateral = new THREE.Object3D();
+    this.posSuperficie = new THREE.Object3D();
+    this.posSuperficie.position.y = circuito.parameters.radius;
 
-    // Se crea una sección para los controles de la caja
-    var folder = gui.addFolder (titleGui);
-    // Estas lineas son las que añaden los componentes de la interfaz
-    // Las tres cifras indican un valor mínimo, un máximo y el incremento
-    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
-    folder.add (this.guiControls, 'sizeX', 0.1, 5.0, 0.01).name ('Tamaño X : ').listen();
-    folder.add (this.guiControls, 'sizeY', 0.1, 5.0, 0.01).name ('Tamaño Y : ').listen();
-    folder.add (this.guiControls, 'sizeZ', 0.1, 5.0, 0.01).name ('Tamaño Z : ').listen();
-    
-    folder.add (this.guiControls, 'rotX', 0.0, Math.PI/2, 0.01).name ('Rotación X : ').listen();
-    folder.add (this.guiControls, 'rotY', 0.0, Math.PI/2, 0.01).name ('Rotación Y : ').listen();
-    folder.add (this.guiControls, 'rotZ', 0.0, Math.PI/2, 0.01).name ('Rotación Z : ').listen();
-    
-    folder.add (this.guiControls, 'posX', -20.0, 20.0, 0.01).name ('Posición X : ').listen();
-    folder.add (this.guiControls, 'posY', 0.0, 10.0, 0.01).name ('Posición Y : ').listen();
-    folder.add (this.guiControls, 'posZ', -20.0, 20.0, 0.01).name ('Posición Z : ').listen();
+    this.add(this.nodoPosOrientTubo);
+    this.nodoPosOrientTubo.add(this.movimientoLateral);
+    this.movimientoLateral.add(this.posSuperficie);
+    this.movimientoLateral.rotateZ(rot);
+    this.posSuperficie.add(this.baseMesh);
+    //pergarlo al tubo
+    this.t = ti;
+    this.tubo = circuito;
+    this.path = circuito.parameters.path;
+    this.radio = circuito.parameters.radius;
+    this.segmentos = circuito.parameters.tubularSegments;
 
-    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
-    }
+    var posTemp = this.path.getPointAt(this.t);
+    this.nodoPosOrientTubo.position.copy(posTemp);
+    var tangente = this.path.getTangentAt(this.t);
+    posTemp.add(tangente);
+    var segmentoActual = Math.floor(this.t * this.segmentos);
+    this.nodoPosOrientTubo.up = this.tubo.binormals[segmentoActual];
+    this.nodoPosOrientTubo.lookAt(posTemp);
+  }
 
-    update () {
-        // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
-        // Primero, el escalado
-        // Segundo, la rotación en Z
-        // Después, la rotación en Y
-        // Luego, la rotación en X
-        // Y por último la traslación
-       
-        this.position.set (this.guiControls.posX,this.guiControls.posY,this.guiControls.posZ);
-        this.scale.set (this.guiControls.sizeX,this.guiControls.sizeY,this.guiControls.sizeZ);
-        this.rotation.set (this.guiControls.rotX,this.guiControls.rotY,this.guiControls.rotZ);
-    }
+  createColision(){
+    var box = new THREE.Box3();
+    box.setFromObject(this.baseMesh);
+    var boxHelper = new THREE.Box3Helper(box, 0xffff00);
+    boxHelper.visible = true;
+    boxHelper.userData = this;
+    this.baseMesh.add(boxHelper);
+  }
 }
 
 export { Impulsor }
